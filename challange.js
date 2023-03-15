bba2ec1c2bde67cd82995360ea715641afa4ab5a
tonyGeorge <tg764562@gmail.com>
2023-03-15 02:27:24 +0200

    V02

--- challange.js
+++ challange.js
@@ -1,25 +1,19 @@
-// Set the date we're counting down to
-var now = new Date().getTime();
-var countDownDate = now+92000;
-// Update the count down every 1 second
-var x = setInterval(function() {
+document.addEventListener('DOMContentLoaded', function() {
+  const timerElement = document.getElementById('timer');
+  const endTime = Date.now() + 10 * 1000; // 10 seconds from now
 
-  // Get today's date and time
-  var now1 = new Date().getTime();
+  function updateTimer() {
+    const remainingTime = Math.max(0, endTime - Date.now());
+    const minutes = Math.floor(remainingTime / 60000);
+    const seconds = Math.floor((remainingTime % 60000) / 1000);
+    timerElement.textContent = `Time remaining: ${minutes}m ${seconds}s`;
 
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now1;
+    if (remainingTime <= 0) {
+      window.location.replace("https://www.google.com");
+    }
+  }
 
-  // Time calculations for days, hours, minutes and seconds
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  updateTimer(); // start the timer
+  setInterval(updateTimer, 1000); // update every second
+});
 
-  // Display the result in the element with id="timer"
-  document.getElementById("timer").innerHTML = 
-  minutes + "m " + seconds + "s ";
-
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    window.location.replace("timeout.html");  }
-}, 1000);
